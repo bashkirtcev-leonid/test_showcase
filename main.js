deb2d57f11684c88c5a20f3b7ab56549ae9e76a5
Башкирцев Леонид Владимирович <leonid.bashkirtcev@domru.ru>
2019-01-16 17:55:28 +0500

    Fix load

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 
-/*var element = document.createElement('div');
-element.id = 'app1';
-document.body.appendChild(element);*/
+var element = document.createElement('div');
+element.id = 'app';
+document.body.appendChild(element);
 
 var app = new Vue({
-	el: '#app1',
-	template: '{{name}}',
+	el: '#app',
 	data: {
 		name: 'example'
+	},
+	render: function (createElement) {
+		return createElement('div', this.name);
 	}
 });
